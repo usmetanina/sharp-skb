71bc874a6b9928da7ad131ba1f465340b8957182
usmetanina <usmetanina96@mail.ru>
2016-09-26 12:56:24 +0500

    Task 1. Work with triangle

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Три стороны \n2. Две стороны и угол \n3. Два угла и сторона \nВсе величины могут быть дробными числами\nВаш выбор:");
+            string choice = Console.ReadLine();
+            Triangle triangle=null;
+
+            if (Convert.ToInt32(choice) < 4 && Convert.ToInt32(choice) > 0)
+            {
+                if (choice.Equals("1")) triangle = new Triangle(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+                if (choice.Equals("2")) triangle = new Triangle(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Console.ReadLine());
+                if (choice.Equals("3")) triangle = new Triangle(Console.ReadLine(), Console.ReadLine(), Convert.ToDouble(Console.ReadLine()));
+
+                if (triangle.GetArea() == 0)
+                    Console.WriteLine("Треугольник не существует");
+                else Console.WriteLine(triangle.GetArea());
+            }
+        }
+    }
+}
---
+++ Task1/Task1/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculation;
+
+namespace WorkWithTriangle
+{
+    class Triangle
+    {
+        private double[] sides;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            sides = CalculationTriangleParameters.CalculationSide(sideA, sideB, sideC);
+        }
+
+        public Triangle(double firstSide, double secondSide, string angle)
+        {
+            sides = CalculationTriangleParameters.CalculationSide(firstSide, secondSide, angle);
+        }
+
+        public Triangle(string firstAngle, string secondAngle, double side)
+        {
+            sides = CalculationTriangleParameters.CalculationSide(firstAngle, secondAngle, side);
+        }
+
+        public double GetArea()
+        {
+            return CalculationTriangleParameters.CalculationArea(sides);
+        }
+    }
+}
