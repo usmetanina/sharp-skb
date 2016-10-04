31d44c5508ff70d0cbc04dd1def321a5b94433ac
usmetanina <usmetanina96@mail.ru>
2016-10-04 21:07:46 +0500

    add IsCellsClose

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -8,8 +8,8 @@ namespace Game
 {
     public class Cell
     {
-        public int X { get; private set; }
-        public int Y { get; private set; }
+        public readonly int X;
+        public readonly int Y;
 
         public Cell(int x, int y)
         {
@@ -64,7 +64,7 @@ namespace Game
 
         private bool IsUniqueAndCorrectCells(int[] cells)
         {
-            int countZero=0;
+            int countZero = 0;
             for (int i = 0; i < cells.Length; i++)
             {
                 if (cells[i] >= 0 && cells[i] < cells.Length)
@@ -80,13 +80,14 @@ namespace Game
                 else
                     throw new ArgumentException("Не корректные значения клеток");
             }
-            if (countZero!=1) throw new ArgumentException("Нет пустой клетки");
+            if (countZero != 1) throw new ArgumentException("Нет пустой клетки");
             else return true;
         }
 
         public int this[int x, int y]
         {
-            get {
+            get
+            {
                 if (x >= 0 && x < sizeField && y >= 0 && y < sizeField)
                     return field[x, y];
                 else throw new ArgumentException("Выход за пределы поля");
@@ -102,71 +103,24 @@ namespace Game
         {
             Cell currentCell = GetLocation(cell);
 
-            if (IsMovementUp(currentCell) || (IsMovementDown(currentCell)) ||
-                IsMovementLeft(currentCell) || IsMovementRight(currentCell))
+            if (IsCellsClose(currentCell, GetLocation(0)))
             {
-                
+
                 field[cellLocation[0].X, cellLocation[0].Y] = cell;
                 field[currentCell.X, currentCell.Y] = 0;
 
-                cellLocation[cell]=GetLocation(0);
+                cellLocation[cell] = GetLocation(0);
                 cellLocation[0] = currentCell;
             }
             else throw new ArgumentException("Двигать клетку невозможно");
         }
 
-        private bool IsMovementUp(Cell cell)
+        private bool IsCellsClose(Cell firsCell, Cell secondCell)
         {
-            if (cell.X != 0)
-            {
-                if (field[cell.X - 1, cell.Y] == 0)
-                    return true;
-                else return false;
-
-            }
-            else
-                return false;
-        }
-
-        private bool IsMovementDown(Cell cell)
-        {
-            if (cell.X != sizeField-1)
-            {
-                if (field[cell.X + 1, cell.Y] == 0)
-                    return true;
-                else return false;
-
-            }
-            else
-                return false;
-        }
-
-        private bool IsMovementLeft(Cell cell)
-        {
-            if (cell.Y != 0)
-            {
-                if (field[cell.X, cell.Y-1] == 0)
-                    return true;
-                else return false;
-
-            }
-            else
-                return false;
-        }
-        private bool IsMovementRight(Cell cell)
-        {
-            if (cell.Y != sizeField-1)
-            {
-                if (field[cell.X, cell.Y + 1] == 0)
-                    return true;
-                else return false;
-
-            }
-            else
-                return false;
+            return ((firsCell.X == secondCell.X) && (Math.Abs(firsCell.Y - secondCell.Y) == 1))
+                 || ((firsCell.Y == secondCell.Y) && (Math.Abs(firsCell.X - secondCell.X) == 1));
         }
     }
-
 
     class Program
     {
