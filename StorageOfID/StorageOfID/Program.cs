a4b5721865d96d75c494a4ce142a175b7d1ff9f2
usmetanina <usmetanina96@mail.ru>
2016-10-21 11:06:43 +0500

    Srorage of guid. Generics

---
+++ StorageOfID/StorageOfID/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageOfID
+{
+    class MyType{}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StorageOfID storage = new StorageOfID();
+            var a = storage.Create<MyType>();
+        }
+    }
+}
---
+++ StorageOfID/StorageOfID/StorageOfID.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageOfID
+{
+    public class StorageOfID
+    {
+        Dictionary<Type, Dictionary<Guid, Object>> storageOfGuid = new Dictionary<Type, Dictionary<Guid, Object>>();
+
+        public T Create<T>()
+            where T : new()
+        {
+            T obj = new T();
+            if (!storageOfGuid.ContainsKey(typeof(T)))
+                storageOfGuid[typeof(T)] = new Dictionary<Guid, Object>();
+
+            storageOfGuid[typeof(T)][Guid.NewGuid()] = obj;
+
+            return obj;
+        }
+
+        public Dictionary<Guid, Object> GetPairsByType<T>()
+        {
+            if (storageOfGuid.ContainsKey(typeof(T)))
+                return storageOfGuid[typeof(T)];
+            else
+                return null;
+        }
+
+        public T GetObjectByGuid<T>(Guid guid)
+        {
+            if (storageOfGuid.ContainsKey(typeof(T)))
+                if (storageOfGuid[typeof(T)].ContainsKey(guid))
+                    return (T)storageOfGuid[typeof(T)][guid];
+
+            return default(T);
+        }
+
+    }
+}
