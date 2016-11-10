f54a269ca99ebb9f05ca4376ab3ff2f7715aeebb
usmetanina <usmetanina96@mail.ru>
2016-11-10 17:05:33 +0500

    MemoryMenegement. part1. Timer

---
+++ MemoryMenegement/MemoryMenegement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MemoryMenegement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+            using (timer.Start())
+            {
+                Thread.Sleep(150);
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            var value = 678+789;
+            Console.WriteLine("тянем время");
+
+            using (timer.Continue())
+            {
+                Thread.Sleep(250);
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ MemoryMenegement/MemoryMenegement/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryMenegement
+{
+    class Timer:  IDisposable
+    {
+        private readonly Stopwatch stopwatch;
+        public long ElapsedMilliseconds
+        {
+            get
+            {
+                return stopwatch.ElapsedMilliseconds;
+            }
+        }
+
+        public Timer()
+         {
+            stopwatch = new Stopwatch();
+        }
+
+        public Timer Start()
+        {
+            stopwatch.Start();
+            return this;
+        }
+
+        public Timer Continue()
+        {
+            stopwatch.Start();
+            return this;
+        }
+
+        public void Dispose()
+        {
+            stopwatch.Stop();
+        }
+    }
+}
