5ee66d14818ce159f54cd74d22aca930bd57c747
usmetanina <usmetanina96@mail.ru>
2016-11-17 22:31:47 +0500

    Files. Part1

---
+++ Files/Files/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var line in Reader.ReadCsv1("airquality.csv"))
+            {
+                foreach (var item in line)
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Files/Files/Reader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Files
+{
+    class Reader
+    {
+        public static IEnumerable<string[]> ReadCsv1(string file)
+        {
+
+            using (StreamReader stream = new StreamReader(file))
+                while (true)
+                {
+                    var currentString = stream.ReadLine();
+                    if (currentString == null)
+                    {
+                        stream.Close();
+                        yield break;
+                    }
+                    var line = currentString.Split(',');
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] == "NA")
+                            line[i] = null;
+                    }
+                    yield return line;
+                }
+        }
+    }
+}
